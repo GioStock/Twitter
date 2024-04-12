3dc6040a8fba0fbb2038122da0d78da5cda69b70
GioStock <149418816+GioStock@users.noreply.github.com>
2024-04-12 08:38:37 +0200

    funzioni corrette

--- twitter/js/twitt.js
+++ twitter/js/twitt.js
@@ -1,28 +1,27 @@
 let utenteOk=document.querySelector("#utenteOk");
 let Logout=document.querySelector("#Logout");
 let btnInviaTweet=document.querySelector("#btnInviaTweet");
-let contatore=document.querySelector("#contatore");
 let textArea=document.querySelector("#textArea");
-
+let contatore=document.querySelector("#contatore");
 btnInviaTweet.addEventListener("click",inserisciTweet);
 
 function inserisciTweet() {
-    
-    let tweetInserito=document.querySelector("#tweetInserito");
-    tweetInserito.innerHTML = `<h6 class="text-start ms-3">Nuovo tweet: "${textArea.value}"</h6>`;
-    
-    
-    textArea.value="";
+  let tweetInserito = document.querySelector("#tweetInserito");
+  tweetInserito.innerHTML = `<h6 class="text-start ms-3">Nuovo tweet: "${textArea.value}"</h6>`;
+
+  //reset area una volta inviato
+  textArea.value = "";
+  //reset contatore una volta inviato
+  contatore.textContent = "0";
 }
+
+// creazione contatore fino a 50
+contatore.textContent="0/50";
+textArea.addEventListener('input',contaCaratteri);
+
 function contaCaratteri() {
-    
-    
     contatore.textContent=textArea.value.length;
-    console.log(contatore.textContent);
     if (contatore.textContent==50) {
         contatore.textContent="limite raggiunto!!!";
     }
-    
-    
 }
-textArea.addEventListener('input',contaCaratteri);

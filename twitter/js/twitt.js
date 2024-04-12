855feb7c24a388b283f902348ae1433d2a52033a
GioStock <149418816+GioStock@users.noreply.github.com>
2024-04-12 08:13:26 +0200

    contaCaratteri Inserito

--- twitter/js/twitt.js
+++ twitter/js/twitt.js
@@ -1,15 +1,28 @@
 let utenteOk=document.querySelector("#utenteOk");
 let Logout=document.querySelector("#Logout");
 let btnInviaTweet=document.querySelector("#btnInviaTweet");
+let contatore=document.querySelector("#contatore");
+let textArea=document.querySelector("#textArea");
 
 btnInviaTweet.addEventListener("click",inserisciTweet);
 
 function inserisciTweet() {
-    let textArea=document.querySelector("#textArea");
+    
     let tweetInserito=document.querySelector("#tweetInserito");
-    tweetInserito.innerHTML = `<h6 class="text-start ms-3">${textArea.value}</h6>`;
+    tweetInserito.innerHTML = `<h6 class="text-start ms-3">Nuovo tweet: "${textArea.value}"</h6>`;
     
     
     textArea.value="";
+}
+function contaCaratteri() {
+    
+    
+    contatore.textContent=textArea.value.length;
+    console.log(contatore.textContent);
+    if (contatore.textContent==50) {
+        contatore.textContent="limite raggiunto!!!";
+    }
+    
     
 }
+textArea.addEventListener('input',contaCaratteri);
